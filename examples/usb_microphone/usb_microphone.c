5528d24ecb19c3c0ab3b063d5d771f8372253629
turtle-beep <michellepaszek@yahoo.com>
2026-01-13 16:51:41 -0500

    Update usb_microphone.c

--- examples/usb_microphone/usb_microphone.c
+++ examples/usb_microphone/usb_microphone.c
@@ -326,12 +326,18 @@ bool tud_audio_tx_done_pre_load_cb(uint8_t rhport, uint8_t itf, uint8_t ep_in, u
   (void) ep_in;
   (void) cur_alt_setting;
 
-  if (usb_microphone_tx_ready_handler)
-  {
-    usb_microphone_tx_ready_handler();
-  }
+  //if (usb_microphone_tx_ready_handler)
+  //{
+    //usb_microphone_tx_ready_handler();
+  //}
 
-  return true;
+  //return true;
+//}
+  if (cur_alt_setting == 1 && usb_microphone_tx_ready_handler)
+    {
+      usb_microphone_tx_ready_handler();
+    }
+    return true;
 }
 
 bool tud_audio_tx_done_post_load_cb(uint8_t rhport, uint16_t n_bytes_copied, uint8_t itf, uint8_t ep_in, uint8_t cur_alt_setting)
