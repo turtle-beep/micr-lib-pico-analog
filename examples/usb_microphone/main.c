109f33e9f2bf5501cbad2d99ad0334944c2247cc
turtle-beep <michellepaszek@yahoo.com>
2026-01-13 15:46:56 -0500

    Update main.c

--- examples/usb_microphone/main.c
+++ examples/usb_microphone/main.c
@@ -12,88 +12,35 @@
  * https://github.com/hathach/tinyusb/tree/master/examples/device/audio_test
  */
 
-//#include "pico/pdm_microphone.h"
 #include "pico/analog_microphone.h"
-
 #include "usb_microphone.h"
 
-const struct analog_microphone_config config = {
-    .gpio = 26,              // ADC-compatible GPIO (26-28)
-    .bias_voltage = 1.25,    // Adjust based on your mic (MAX9814 typically 1.25V)
-    .sample_rate = 48000,    // 48 kHz
-    .sample_buffer_size = 256,
+static int16_t sample_buffer[SAMPLES_PER_FRAME];
+
+const struct analog_microphone_config config =
+{
+  .gpio = 26,
+  .bias_voltage = 1.25,
+  .sample_rate = SAMPLE_RATE,
+  .sample_buffer_size = SAMPLES_PER_FRAME
 };
 
-// configuration
-
-//const struct pdm_microphone_config config = {
-//  .gpio_data = 2,
-//  .gpio_clk = 3,
-//  .pio = pio0,
-//  .pio_sm = 0,
-//  .sample_rate = SAMPLE_RATE,
-//  .sample_buffer_size = SAMPLE_BUFFER_SIZE,
-//};
-
-// variables
-uint16_t sample_buffer[SAMPLE_BUFFER_SIZE];
-
-// callback functions
-//void on_pdm_samples_ready();
-void on_analog_samples_ready();
-void on_usb_microphone_tx_ready();
+void on_samples_ready(void)
+{
+  analog_microphone_read(sample_buffer, SAMPLES_PER_FRAME);
+  usb_microphone_write(sample_buffer);
+}
 
 int main(void)
 {
-  //initialize and start the analog microphone
   analog_microphone_init(&config);
-  analog_microphone_set_samples_ready_handler(on_analog_samples_ready);
+  analog_microphone_set_samples_ready_handler(on_samples_ready);
   analog_microphone_start();
-  
-  
-  
-  // initialize and start the PDM microphone
-  //pdm_microphone_init(&config);
-  //pdm_microphone_set_samples_ready_handler(on_pdm_samples_ready);
-  //pdm_microphone_start();
-  
 
-  // initialize the USB microphone interface
   usb_microphone_init();
-  usb_microphone_set_tx_ready_handler(on_usb_microphone_tx_ready);
 
-  while (1) {
-    // run the USB microphone task continuously
+  while (1)
+  {
     usb_microphone_task();
   }
-
-  return 0;
-}
-
-//void on_pdm_samples_ready()
-//{
-  // Callback from library when all the samples in the library
-  // internal sample buffer are ready for reading.
-  //
-  // Read new samples into local buffer.
-  //pdm_microphone_read(sample_buffer, SAMPLE_BUFFER_SIZE);
-//}
-
-void on_analog_samples_ready()
-{
-  // Callback from library when all the samples in the library
-  // internal sample buffer are ready for reading.
-  //
-  // Read new samples into local buffer.
-  analog_microphone_read(sample_buffer, SAMPLE_BUFFER_SIZE);
-}
-
-
-void on_usb_microphone_tx_ready()
-{
-  // Callback from TinyUSB library when all data is ready
-  // to be transmitted.
-  //
-  // Write local buffer to the USB microphone
-  usb_microphone_write(sample_buffer, sizeof(sample_buffer));
 }
