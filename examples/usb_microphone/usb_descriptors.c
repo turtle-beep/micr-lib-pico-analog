e267367bd4c866c794d17384945a56ad884da9ba
turtle-beep <michellepaszek@yahoo.com>
2026-01-15 14:18:53 -0500

    Update usb_descriptors.c

--- examples/usb_microphone/usb_descriptors.c
+++ examples/usb_microphone/usb_descriptors.c
@@ -24,7 +24,7 @@
  */
 
 #include "tusb.h"
-#include "class/audio/audio.h"
+#include "tusb.h/class/audio/audio.h"
 
 /* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
  * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
